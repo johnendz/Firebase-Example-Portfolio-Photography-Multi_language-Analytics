64a910de55902995380340e0dbf6392431f8e7dc
Alexander Friedrich <johnendz@icloud.com>
2019-08-15 12:40:56 -0300

    Update languages.js

--- public/painel/languages.js
+++ public/painel/languages.js
@@ -29,8 +29,8 @@ db.collection("devices")
 	});
 	var counts = {};
 	for (var i = 0; i < arr.length; i++) {
-	  var num = arr[i];
-	  counts[num] = counts[num] ? counts[num] + 1 : 1;
+        var num = arr[i];
+        	counts[num] = counts[num] ? counts[num] + 1 : 1;
 	}
 	let result = arr.sort().reduce((init, current) => {
 	    if (init.length === 0 || init[init.length - 1] !== current) {
